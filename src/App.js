63babf34413dbf37081f9cbd58d3c8a982b76307
Roccoz <zagarocco9@outlook.it>
2023-11-14 14:44:14 +0100

    Put a constructor with this.state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+   super();
+
+   this.state = {
+     name : 'Rocco'
+   }
+  }
+
   render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello my name is Rocco. Ciao Fabio!
+            Hello my name is {this.state.name}. Ciao Fabio!
           </p>
           <button> Change name</button>
         </header>
