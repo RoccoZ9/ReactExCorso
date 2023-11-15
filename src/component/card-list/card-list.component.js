f3eba384a9121a96d504f520f7b8dddaf105f6a4
Roccoz <zagarocco9@outlook.it>
2023-11-15 14:21:55 +0100

    add a style to the card and fix the code

--- src/component/card-list/card-list.component.js
+++ src/component/card-list/card-list.component.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-
+import './card.css';
+import './card-list.css';
 class CardList extends Component {
     render() {
         console.log('render from CardList');
         const { monsters } = this.props;
-
         return (
-            <div>
-                <h1>Ciao Fabio</h1>
-                {monsters.map((monster) => (
-                    <h2 key={monster.id}>{monster.name}</h2>
-                ))}
+            <div className='card-list'>
+                {monsters.map((monster) => {
+                    const { name, id, email } = monster;
+                    return (
+                        <div className='card-container' key={id}>
+                            <h1>Ciao Fabio</h1>
+                            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    )
+                })}
             </div>
         );
     }
