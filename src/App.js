3c62067ddd905fe12392be0286616a4282f4136f
Roccoz <zagarocco9@outlook.it>
2023-11-15 10:05:35 +0100

    add a filter

--- src/App.js
+++ src/App.js
@@ -29,6 +29,18 @@ class App extends Component {
     console.log('render');
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={
+          (event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filterMonster = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString)
+            });
+
+            this.setState(() => {
+              return { monsters: filterMonster };
+            })
+
+          }} />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
